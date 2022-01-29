188c960e919a19ec358a2aea7a4426f3a90e6aa6
Dado Marti <dado@paf.hr>
2022-01-29 17:26:00 +0100

    Insert, edit pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,31 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description, :image_url))
-    @article.save
-    redirect_to article_path(@article)
+    if @article.save
+      flash[:notice] = "Article was createtd successfuly!"
+      redirect_to article_path(@article)
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description, :image_url))
+      flash[:notice] = "Article was updated."
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
 end
