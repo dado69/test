dedb33082614008792fc023d1b8a305844b8485d
Dado Marti <dado@paf.hr>
2022-02-08 14:52:31 +0100

    Add users create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -43,4 +43,14 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :decription)
+  end
+
 end
